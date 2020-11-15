97513df1f3bb629d6c11f3a9db3fefe2dd4df8dd
malhar7331 <74482897+malhar7331@users.noreply.github.com>
2020-11-15 11:17:56 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDUBWR9zh7YOeNW7dnj7ocFYntoaqHNi8M",
+    authDomain: "let-s-chat-a319d.firebaseapp.com",
+    databaseURL: "https://let-s-chat-a319d.firebaseio.com",
+    projectId: "let-s-chat-a319d",
+    storageBucket: "let-s-chat-a319d.appspot.com",
+    messagingSenderId: "433471315109",
+    appId: "1:433471315109:web:e0f7812774e2d6d445c14c",
+    measurementId: "G-ZBZ51JJ0P6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
